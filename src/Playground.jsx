2e9b57e81c37aa3bbb8a30d693b318117a480f2b
Saurav Dutta <saurav@piktorlabs.com>
2021-07-14 17:57:10 +0530

    adding diagram to yaml

--- src/Playground.jsx
+++ src/Playground.jsx
@@ -3,14 +3,14 @@ import "./Playground.css";
 import Box from "./components/Box";
 import TopBar from "./components/TopBar";
 import XArrow from "./components/Xarrow";
-
-const shapes = ["wideBox"];
+const YAML = require("json-to-pretty-yaml");
 
 const images = [
   {
     id: "1",
     name: "S3",
     type: "wideBox",
+    friendlyName: "S3 Bucket",
     img:
       "https://cdn.iconscout.com/icon/free/png-512/amazon-s3-2968702-2464706.png"
   },
@@ -18,12 +18,14 @@ const images = [
     id: "2",
     name: "Cloudfront",
     type: "wideBox",
+    friendlyName: "Cloudfront Distribution",
     img: "https://logowik.com/content/uploads/images/aws-cloudfront6788.jpg"
   },
   {
     id: "3",
     name: "Users",
     type: "wideBox",
+    friendlyName: "Users/Client",
     img: "https://static.thenounproject.com/png/428433-200.png"
   }
 ];
@@ -44,6 +46,7 @@ const PlayGround = () => {
 
   const [boxes, setBoxes] = useState([]);
   const [lines, setLines] = useState([]);
+  const [ymlVal, setYmlVal] = useState("");
 
   const [selected, setSelected] = useState(null);
   const [actionState, setActionState] = useState("Normal");
@@ -67,11 +70,41 @@ const PlayGround = () => {
           x: e.clientX - x,
           y: e.clientY - y,
           shape,
-          img: result[0].img
+          img: result[0].img,
+          friendlyName: result[0].friendlyName
         };
         setBoxes([...boxes, newBox]);
       }
     }
+  };
+
+  const generateYml = () => {
+    let models = [];
+    lines.map((item, index) => {
+      let startKey = item.props.start;
+      let startObj = {
+        uniqueName: startKey,
+        friendlyName: boxes.find(item => item.id === startKey).friendlyName
+      };
+      if (startKey !== "Users") {
+        startKey = "Component";
+      }
+      models.push({ [startKey]: startObj });
+
+      if (index === lines.length - 1) {
+        let endKey = item.props.end;
+        let endObj = {
+          uniqueName: endKey,
+          friendlyName: boxes.find(item => item.id === endKey).friendlyName
+        };
+        if (endKey !== "Users") {
+          endKey = "Component";
+        }
+        models.push({ [endKey]: endObj });
+      }
+    });
+    let yamlValue = YAML.stringify({ model: models });
+    setYmlVal(yamlValue);
   };
 
   const props = {
@@ -152,6 +185,13 @@ const PlayGround = () => {
             setSelected={setSelected}
           />
         ))}
+      </div>
+
+      <div className="scriptContainer">
+        <button onClick={generateYml}>Generate Yml</button>
+        <div>
+          <pre>{ymlVal}</pre>
+        </div>
       </div>
     </div>
   );
